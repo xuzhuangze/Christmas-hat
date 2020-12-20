08462b532587958027ef5e989a544b9cd6cea32b
xuzhuanzge <1733150393@qq.com>
2020-12-20 22:13:22 +0800

    12.20 22:12dev分支提交，能画上圣诞帽了

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,12 +1,4 @@
-// const db = wx.cloud.database();
-// const findByWhere = (table, where) => {
-//   return db.collection(table).where(where).get();
-// }
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
     userInfo: {},
     ctx: '',
@@ -23,52 +15,41 @@ Page({
     uuuurl: '',
     headPic: '',
     hasUploadHeadpic: 0,
-    OffscreenCanvas: '',
     canvasContext: '',
     hats: []
   },
 
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
+    this.setData({ canvasContext: wx.createCanvasContext('canvas') });
     this._getHats();
-    this.setData({ OffscreenCanvas: wx.createOffscreenCanvas() });
-    this.setData({ canvasContext: wx.createCanvasContext('canvas', this.data.OffscreenCanvas) });
   },
+
+  // 获取帽子列表
   async _getHats() {
     let res = await wx.cloud.database().collection('hatList').where({ status: 1 }).get();
-    console.log(res);
+    // console.log(res);
     if (res.data && res.data.length != 0) {
       this.setData({ hats: res.data });
     }
   },
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
 
-  },
-
-
-  async _useCanvas(url) {
+  // 画布
+  async _useCanvas(url, x = 0, y = 0, w = 412, h = 412) {
+    this.setData({ canvasContext: wx.createCanvasContext('canvas') });
     let dw = 375;
     await wx.getSystemInfo({
       success(res) {
-        console.log(res.safeArea.width);
         dw = res.safeArea.width;
       }
     })
     // 根据屏幕尺寸计算图片的px值
     let rpx = dw / 750;
-    let pw = 412 * rpx;
-    let ph = 412 * rpx;
-    console.log(pw, 'pw');
-    console.log(ph, 'ph');
+    let pw = w * rpx;
+    let ph = h * rpx;
 
-    this.data.canvasContext.drawImage(url, 0, 0, pw, ph)
-    this.data.canvasContext.draw();
+    this.data.canvasContext.beginPath();
+    this.data.canvasContext.drawImage(url, x, y, pw, ph)
+    this.data.canvasContext.draw(true);
     // this.data.canvasContext.draw(true, () => { 
     //   setTimeout(() => {
     //     wx.canvasToTempFilePath({
@@ -92,22 +73,53 @@ Page({
     // });
   },
 
+  // 保存结果
+  _saveResult() {
+    this.data.canvasContext.drawImage('', -1, -1, 0, 0)
+    this.data.canvasContext.draw(true, () => {
+      setTimeout(() => {
+        wx.canvasToTempFilePath({
+          x: 0, y: 0,
+          width: 412,
+          height: 412,
+          destWidth: 412,
+          destHeight: 412,
+          canvasId: 'canvas',
+          fileType: 'jpg',
+          quality: 1,
+          success: (res) => {
+            this.uuuurl = res.tempFilePath;
+            console.log(res.tempFilePath);
+            if (res.tempFilePath) {
+              wx.showToast({
+                title: '图片下载到' + res.tempFilePath,
+              })
+            }
+          },
+          fail(res) {
+            console.log(res, 'fail');
+          }
+        })
+      }, 1000)
+    });
+  },
 
 
   // 获取头像并调用canvas函数将头像画在画布中
   _getUserInfo(e) {
-    console.log(e.detail.userInfo, 111);
+    // console.log(e.detail.userInfo, 111);
     if (e.detail.userInfo) {
       this.setData({
         userInfo: e.detail.userInfo,
         headPic: e.detail.userInfo.avatarUrl,
         hasUploadHeadpic: 1
       });
-      wx.downloadFile({
-        url: this.data.headPic,
+      console.log(this.data.headPic);
+      wx.getImageInfo({
+        src: this.data.headPic,
         success: (res) => {
           console.log(res);
-          this._useCanvas(res.tempFilePath);//参数为头像的路径
+          this._useCanvas(res.path);//参数为头像的路径
         }
       })
     } else {
@@ -120,8 +132,18 @@ Page({
 
 
 
+  // 画帽子
+  _drawHat(e) {
+    wx.getImageInfo({
+      src: e.currentTarget.dataset.path,
+      success: (res) => {
+        this._useCanvas(res.path, 60, -8, 200, 200);//参数为头像的路径
+      }
+    })
+  },
 
-  
+
+
   /**
    * 生命周期函数--监听页面显示
    */
